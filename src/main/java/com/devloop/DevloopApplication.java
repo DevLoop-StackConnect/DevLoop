a32c2495277ad830c7c2f1910d84f616333ff7e3
macbookpro <ks12467@naver.com>
2024-10-21 14:54:16 +0900

    first commit

---
+++ src/main/java/com/devloop/DevloopApplication.java
@@ -0,0 +1,13 @@
+package com.devloop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevloopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevloopApplication.class, args);
+	}
+
+}
