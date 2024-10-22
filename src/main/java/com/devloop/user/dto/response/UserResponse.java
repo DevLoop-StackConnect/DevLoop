d850140b35e149cc0226232241a52c5c4103ce2c
Ackrilll <skawlsgus2@naver.com>
2024-10-23 00:35:10 +0900

    [refactor] UserResponse 생성자 private, from 메서드로 받도록 캡슐화

--- src/main/java/com/devloop/user/dto/response/UserResponse.java
+++ src/main/java/com/devloop/user/dto/response/UserResponse.java
@@ -9,9 +9,13 @@ public class UserResponse {
     private final String userEmail;
     private final UserRole userRole;
 
-    public UserResponse(final String userName, final String userEmail, final UserRole userRole) {
+    private UserResponse(final String userName, final String userEmail, final UserRole userRole) {
         this.userName = userName;
         this.userEmail = userEmail;
         this.userRole = userRole;
+    }
+
+    public static UserResponse from(final String userName, final String userEmail, final UserRole userRole) {
+        return new UserResponse(userName, userEmail, userRole);
     }
 }
