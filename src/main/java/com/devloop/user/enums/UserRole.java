cc1c445ba09fe9128c005239560e3c986d2dc1be
macbookpro <ks12467@naver.com>
2024-10-22 19:23:18 +0900

    feat/auth/fix : auth 수정 및 테스트 완료

--- src/main/java/com/devloop/user/enums/UserRole.java
+++ src/main/java/com/devloop/user/enums/UserRole.java
@@ -1,22 +1,32 @@
 package com.devloop.user.enums;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
 import java.util.Arrays;
 
+
+@Getter
+@RequiredArgsConstructor
 public enum UserRole {
 
-    USER, TUTOR, ADMIN;
+    ROLE_USER(authority.USER),
+    ROLE_TUTOR(authority.TUTOR),
+    ROLE_ADMIN(authority.ADMIN);
+
+    private final String role;
 
     public static UserRole of(String role) {
-        return Arrays.stream(UserRole.values())
+        return Arrays.stream(com.devloop.user.enums.UserRole.values())
                 .filter(r -> r.name().equalsIgnoreCase(role))
                 .findFirst()
                 .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 권한입니다"));
     }
 
-    public static class Authority {
-        public static final String USER = "USER";
-        public static final String TUTOR = "TUTOR";
-        public static final String ADMIN = "ADMIN";
+    public static class authority {
+        public static final String USER = "ROLE_USER";
+        public static final String TUTOR = "ROLE_TUTOR";
+        public static final String ADMIN = "ROLE_ADMIN";
     }
 }
 
