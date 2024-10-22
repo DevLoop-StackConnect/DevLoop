82cd5ea52b6a4f61dccfbf07b7b9ad69d4ed1a38
kang-sumin <min001117@gmail.com>
2024-10-22 22:54:09 +0900

    [FEAT] 튜터신청 API

--- src/main/java/com/devloop/common/apipayload/status/ErrorStatus.java
+++ src/main/java/com/devloop/common/apipayload/status/ErrorStatus.java
@@ -47,9 +47,13 @@ public enum ErrorStatus implements BaseCode {
     // 동시성 제어
     _CONCURRENT_UPDATE(HttpStatus.CONFLICT, "409", "동시성 업데이트 충돌이 발생했습니다."),
 
+
+    // tutor
+    _HAS_NOT_ACCESS_PERMISSION(HttpStatus.CONFLICT, "409", "해당 서비스 사용 권한이 없습니다."),
+    _TUTOR_REQUEST_ALREADY_EXIST(HttpStatus.CONFLICT, "409", "이미 튜터 요청 신청 내역이 존재합니다."),
+
     //Auth
     _NOT_AUTHENTICATIONPRINCIPAL_USER(HttpStatus.UNAUTHORIZED, "401", "인증되지 않은 유저입니다.");
-
 
 
     private HttpStatus httpStatus;
