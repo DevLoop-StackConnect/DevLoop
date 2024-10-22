6d48a2e58d9951645cced532c6533f27eb66f7f2
jiyumi00 <101707266+jiyumi00@users.noreply.github.com>
2024-10-22 15:59:06 +0900

    Revert "feat/auth/create : 로컬 회원가입, 로그인, 회원탈퇴 API 기능 구현"

--- src/main/java/com/devloop/auth/request/SignoutRequest.java
+++
@@ -1,8 +0,0 @@
-package com.devloop.auth.request;
-
-import lombok.Getter;
-
-@Getter
-public class SignoutRequest {
-    private String password;
-}
--- src/main/java/com/devloop/common/apipayload/BaseCode.java
+++
@@ -1,9 +0,0 @@
-package com.devloop.common.apipayload;
-
-
-
-import com.devloop.common.apipayload.dto.ReasonDto;
-
-public interface BaseCode {
-    public ReasonDto getReasonHttpStatus();
-}
