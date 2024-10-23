a260edbb84f60a1ec350a6f5ccf518b484b26448
goodsoundisme <sol000619@naver.com>
2024-10-23 17:18:18 +0900

    [REFACTOR] Community쪽 컨벤션에 맞게 response수정 및 그에 따른 서비스 코드 수정

--- src/main/java/com/devloop/community/dto/response/CommunityDetailResponse.java
+++ src/main/java/com/devloop/community/dto/response/CommunityDetailResponse.java
@@ -1,6 +1,7 @@
 package com.devloop.community.dto.response;
 
 import com.devloop.common.enums.Category;
+import com.devloop.community.entity.Community;
 import com.devloop.community.entity.ResolveStatus;
 import com.devloop.communitycomment.dto.CommentResponse;
 import lombok.Getter;
@@ -19,7 +20,7 @@ public class CommunityDetailResponse {
     private final Category category;
     private final List<CommentResponse> comments;
 
-    public CommunityDetailResponse(Long communityId, String title, String content, LocalDateTime createdAt, LocalDateTime modifiedAt, ResolveStatus status, Category category, List<CommentResponse> comments) {
+    private CommunityDetailResponse(Long communityId, String title, String content, LocalDateTime createdAt, LocalDateTime modifiedAt, ResolveStatus status, Category category, List<CommentResponse> comments) {
         this.communityId = communityId;
         this.title = title;
         this.content = content;
@@ -28,5 +29,19 @@ public class CommunityDetailResponse {
         this.status = status;
         this.category = category;
         this.comments = comments;
+    }
+
+    //코드컨벤션에 맞춰서 정적 팩토리 메서드 추가
+    public static CommunityDetailResponse from(Community community, List<CommentResponse> comments) {
+        return new CommunityDetailResponse(
+                community.getId(),
+                community.getTitle(),
+                community.getContent(),
+                community.getCreatedAt(),
+                community.getModifiedAt(),
+                community.getResolveStatus(),
+                community.getCategory(),
+                comments
+        );
     }
 }
