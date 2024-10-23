587c13c9df2d910b8ba9a7bdb422a8f4d50b9756
kang-sumin <min001117@gmail.com>
2024-10-23 14:08:22 +0900

    [REFACTOR] TutorRequest.java 어노테이션 수정 관련 import 문 최적화

--- src/main/java/com/devloop/tutor/entity/TutorRequest.java
+++ src/main/java/com/devloop/tutor/entity/TutorRequest.java
@@ -5,10 +5,7 @@ import com.devloop.tutor.enums.TutorRequestStatus;
 import com.devloop.user.entity.User;
 import jakarta.persistence.*;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
-import lombok.Setter;
-import org.springframework.web.bind.annotation.RestController;
 
 @Entity
 @Getter
