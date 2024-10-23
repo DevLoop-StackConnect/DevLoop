9e620ad56cca83e9a042713ed8ca62455edb0f74
goodsoundisme <sol000619@naver.com>
2024-10-23 11:49:05 +0900

    [FEAT] 개발커뮤 게시글 작성 구현

---
+++ src/main/java/com/devloop/community/dto/request/CommunitySaveRequest.java
@@ -0,0 +1,17 @@
+package com.devloop.community.dto.request;
+
+import com.devloop.community.entity.Category;
+import com.devloop.community.entity.ResolveStatus;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@RequiredArgsConstructor
+public class CommunitySaveRequest {
+    private String title;
+    private String content;
+    private ResolveStatus status;
+    private Category category;//게시글 해결 상태 (SOLVED, UNSOLVED)
+}
