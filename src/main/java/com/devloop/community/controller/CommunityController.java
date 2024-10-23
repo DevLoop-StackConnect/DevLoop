fa9626bbbeb9b4c9a520e10ee646dbc613f628ad
goodsoundisme <sol000619@naver.com>
2024-10-23 17:46:13 +0900

    [REFACTOR] url수정

--- src/main/java/com/devloop/community/controller/CommunityController.java
+++ src/main/java/com/devloop/community/controller/CommunityController.java
@@ -17,20 +17,19 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/v1/communities")
+@RequestMapping("/api")
 public class CommunityController {
     private final CommunityService communityService;
 
     //게시글 작성
-    @PostMapping
+    @PostMapping("/v1/communities")
     public ResponseEntity<CommunitySaveResponse> createCommunity(@AuthenticationPrincipal AuthUser authUser, @RequestBody CommunitySaveRequest communitySaveRequest){
-        System.out.println("표시");
         CommunitySaveResponse communitySaveResponse = communityService.createCommunity(authUser,communitySaveRequest);
         return ResponseEntity.ok(communitySaveResponse);
     }
 
     //게시글 다건 조회
-    @GetMapping
+    @GetMapping("/v1/communities")
     public ResponseEntity<Page<CommunitySimpleResponse>> getCommunities(@RequestParam(defaultValue = "0")int page,@RequestParam(defaultValue = "10")int size){
         Pageable pageable = PageRequest.of(page, size);
         Page<CommunitySimpleResponse> communitySimpleResponse = communityService.getCommunities(pageable);
@@ -38,21 +37,21 @@ public class CommunityController {
     }
 
     //게시글 단건 조회
-    @GetMapping("/{communityId}")
+    @GetMapping("/v1/communities/{communityId}")
     public ResponseEntity<CommunityDetailResponse> getCommunity(@PathVariable Long communityId){
         CommunityDetailResponse communityDetailResponse = communityService.getCommunity(communityId);
         return ResponseEntity.ok(communityDetailResponse);
     }
 
     //게시글 수정
-    @PatchMapping("/{communityId}")
+    @PatchMapping("/v1/communities/{communityId}")
     public  ResponseEntity<CommunityDetailResponse> updateCommunity(@PathVariable Long communityId, @RequestBody CommunityUpdateRequest communityUpdateRequest){
         CommunityDetailResponse communityDetailResponse = communityService.updateCommunity(communityId,communityUpdateRequest);
         return ResponseEntity.ok(communityDetailResponse);
     }
 
     //게시글 삭제
-    @DeleteMapping("/{communityId}")
+    @DeleteMapping("/v1/communities/{communityId}")
     public ResponseEntity<Void> deleteCommunity(@PathVariable Long communityId){
         communityService.deleteCommunity(communityId);
         return ResponseEntity.noContent().build();
