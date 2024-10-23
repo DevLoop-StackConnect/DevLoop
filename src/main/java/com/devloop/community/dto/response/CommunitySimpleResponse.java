e57592956135469b1e8a8b23e6e0141c952045d8
goodsoundisme <sol000619@naver.com>
2024-10-23 13:31:25 +0900

    [FIX] 개발커뮤 게시글 다건 조회 기능

---
+++ src/main/java/com/devloop/community/dto/response/CommunitySimpleResponse.java
@@ -0,0 +1,26 @@
+package com.devloop.community.dto.response;
+
+import com.devloop.community.entity.Category;
+import com.devloop.community.entity.ResolveStatus;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+public class CommunitySimpleResponse {
+    private final Long communityId;
+    private final String title;
+    private final LocalDateTime createdAt;
+    private final LocalDateTime modifiedAt;
+    private final ResolveStatus status;
+    private final Category category;
+
+    public CommunitySimpleResponse(Long communityId, String title, LocalDateTime createdAt, LocalDateTime modifiedAt, ResolveStatus status, Category category) {
+        this.communityId = communityId;
+        this.title = title;
+        this.createdAt = createdAt;
+        this.modifiedAt = modifiedAt;
+        this.status = status;
+        this.category = category;
+    }
+}
--- src/main/java/com/devloop/community/service/CommunityService.java
+++ src/main/java/com/devloop/community/service/CommunityService.java
@@ -2,14 +2,22 @@ package com.devloop.community.service;
 
 import com.devloop.common.AuthUser;
 import com.devloop.community.dto.request.CommunitySaveRequest;
+import com.devloop.community.dto.response.CommunityDetailResponse;
 import com.devloop.community.dto.response.CommunitySaveResponse;
+import com.devloop.community.dto.response.CommunitySimpleResponse;
 import com.devloop.community.entity.Community;
 import com.devloop.community.repository.CommunityRepository;
 import com.devloop.user.entity.User;
 import com.devloop.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -39,4 +47,29 @@ public class CommunityService {
                 savedCommunity.getModifiedAt()
         );
     }
+
+    //게시글 다건 조회
+    public Page<CommunitySimpleResponse> getCommunities(Pageable pageable) {
+        //페이지네이션된 게시글 조회
+        Page<Community> communities = communityRepository.findAll(pageable);
+
+        List<CommunitySimpleResponse> responseList = new ArrayList<>();
+        //응답반환
+        for (Community community : communities){
+            CommunitySimpleResponse response = new CommunitySimpleResponse(
+                    community.getId(),
+                    community.getTitle(),
+                    community.getCreatedAt(),
+                    community.getModifiedAt(),
+                    community.getResolveStatus(),
+                    community.getCategory()
+            );
+            responseList.add(response);
+        }
+        return new PageImpl<>(responseList,pageable,communities.getTotalElements());
+    }
+
+//    //게시글 단건(상세조회)
+//    public CommunityDetailResponse getCommunity(Long communityId) {
+//    }
 }
