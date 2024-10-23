bdc0b2febbf0ea2537a31b60cbcf21e0c0356ad6
Ackrilll <skawlsgus2@naver.com>
2024-10-24 01:04:43 +0900

    [refactor] 유저 프로필 조회 항목 추가

--- src/main/java/com/devloop/party/repository/PartyRepository.java
+++ src/main/java/com/devloop/party/repository/PartyRepository.java
@@ -5,9 +5,12 @@ import com.devloop.partycomment.entity.PartyComment;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface PartyRepository extends JpaRepository<Party,Long>{
 
     Page<Party> findByTitleContaining(String title, PageRequest pageable);
 
+    @Query("SELECT p FROM Party p WHERE p.user.id = :userId")
+    Party findByUserId(Long userId);
 }
