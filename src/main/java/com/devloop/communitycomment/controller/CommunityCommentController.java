58a40af8501412f0c13c13eb2dbcb55a5fa22355
goodsoundisme <sol000619@naver.com>
2024-10-23 14:56:31 +0900

    [FEAT] 개발커뮤댓글 url 수정

--- src/main/java/com/devloop/communitycomment/controller/CommunityCommentController.java
+++ src/main/java/com/devloop/communitycomment/controller/CommunityCommentController.java
@@ -16,33 +16,33 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/communities/{communityId}/comments")
+@RequestMapping("/api/v1/communities/{communityId}/comments")
 public class CommunityCommentController {
     private final CommunityCommentService communityCommentService;
 
     //댓글 작성
-    @PostMapping("/v1")
+    @PostMapping
     public ResponseEntity<CommentSaveResponse> createComment(@AuthenticationPrincipal AuthUser authUser, @RequestBody CommentSaveRequest commentSaveRequest, @PathVariable Long communityId){
        CommentSaveResponse commentSaveResponse = communityCommentService.creatComment(authUser,commentSaveRequest,communityId);
        return ResponseEntity.ok(commentSaveResponse);
     }
 
     //댓글 수정
-    @PatchMapping("/v1/{commentId}")
+    @PatchMapping("/{commentId}")
     public ResponseEntity<CommentUpdateResponse> updateComment(@AuthenticationPrincipal AuthUser authUser, @RequestBody CommentUpdateRequest commentUpdateRequest, @PathVariable Long communityId, @PathVariable Long commentId){
         CommentUpdateResponse commentUpdateResponse = communityCommentService.updateComment(authUser,commentUpdateRequest,communityId,commentId);
         return  ResponseEntity.ok(commentUpdateResponse);
     }
 
     //댓글 삭제
-    @DeleteMapping("/v1/{commentId}")
+    @DeleteMapping("/{commentId}")
     public ResponseEntity<Void> deleteComment(@AuthenticationPrincipal AuthUser authUser, @PathVariable Long communityId, @PathVariable Long commentId){
         communityCommentService.deleteComment(authUser,communityId,commentId);
         return ResponseEntity.ok().build();
     }
 
     //댓글 조회
-    @GetMapping("/v1")
+    @GetMapping
     public ResponseEntity<List<CommentResponse>> getComments(@PathVariable Long communityId ){
         List<CommentResponse> comments = communityCommentService.getComments(communityId);
         return ResponseEntity.ok(comments);
