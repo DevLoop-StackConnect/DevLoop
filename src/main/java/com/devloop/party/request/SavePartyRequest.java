30239984f17d19337bae54984b3883975cba2124
jiyumi <jiyumi00@naver.com>
2024-10-23 14:15:48 +0900

    refactor : 코드 리팩토링

--- src/main/java/com/devloop/party/request/SavePartyRequest.java
+++ src/main/java/com/devloop/party/request/SavePartyRequest.java
@@ -1,12 +1,17 @@
 package com.devloop.party.request;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
 public class SavePartyRequest {
+
+    @NotBlank(message = "제목을 작성해 주세요")
     private String title;
+    @NotBlank(message = "내용을 작성해 주세요")
     private String contents;
+    @NotBlank(message = "상태를 작성해 주세요")
     private String status;
 }
