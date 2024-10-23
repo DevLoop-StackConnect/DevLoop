4d355e66319758fb5adf079f462df4210d57d519
kang-sumin <min001117@gmail.com>
2024-10-23 13:03:30 +0900

    [FEAT] 튜터 신청 승인 (ADMIN)

--- src/main/java/com/devloop/tutor/service/TutorAdminService.java
+++ src/main/java/com/devloop/tutor/service/TutorAdminService.java
@@ -7,6 +7,9 @@ import com.devloop.tutor.entity.TutorRequest;
 import com.devloop.tutor.enums.TutorRequestStatus;
 import com.devloop.tutor.repository.TutorRequestRepository;
 import com.devloop.tutor.response.TutorRequestResponse;
+import com.devloop.user.entity.User;
+import com.devloop.user.enums.UserRole;
+import com.devloop.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -20,6 +23,7 @@ import org.springframework.transaction.annotation.Transactional;
 public class TutorAdminService {
 
     private final TutorRequestRepository requestRepository;
+    private final UserRepository userRepository;
 
     // 튜터 신청 요청 조회 (ADMIN : 승인되지 않은 튜터 신청 요청 다건 조회)
     public Page<TutorRequestResponse> getAllTutorRequest(int page, int size) {
@@ -33,5 +37,22 @@ public class TutorAdminService {
         return requests.map(TutorRequestResponse::from);
     }
 
+    // 튜터 신청 승인 (ADMIN : 튜터로 사용자 권한 변경)
+    @Transactional
+    public String changeUserRoleToTutor(Long userId) {
+        // 사용자 객체 가져오기
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new ApiException(ErrorStatus._NOT_FOUND_USER));
 
+        // 튜터 신청 내역 가져오기
+        TutorRequest tutorRequest = requestRepository.findByUserId(userId)
+                .orElseThrow(() -> new ApiException(ErrorStatus._TUTOR_REQUEST_NOT_EXIST));
+
+        // 사용자 권한 변경
+        user.changeUserRoleToTutor(UserRole.ROLE_TUTOR);
+        // 튜터 신청 내역에서 승인여부 승인으로 변경
+        tutorRequest.changeStatus(TutorRequestStatus.APPROVED);
+
+        return String.format("%s 님의 튜터 신청이 승인되었습니다.", user.getUsername());
+    }
 }
