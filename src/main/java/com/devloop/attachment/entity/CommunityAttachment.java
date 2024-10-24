cc3cb3426f8eaea63cac9c298ef482bcc73b554e
Ackrilll <skawlsgus2@naver.com>
2024-10-24 11:55:51 +0900

    [Add] 자식 클래스 생성자 추가

--- src/main/java/com/devloop/attachment/entity/CommunityAttachment.java
+++ src/main/java/com/devloop/attachment/entity/CommunityAttachment.java
@@ -1,13 +1,23 @@
 package com.devloop.attachment.entity;
 
+import com.devloop.attachment.enums.Domain;
+import com.devloop.attachment.enums.FileFormat;
 import jakarta.persistence.DiscriminatorValue;
 import jakarta.persistence.Entity;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import java.net.URL;
 
 @Getter
 @NoArgsConstructor
 @Entity
 @DiscriminatorValue("C")
 public class CommunityAttachment extends Attachment {
+    private CommunityAttachment(URL imageURL, FileFormat fileFormat, Domain domain,String fileName){
+        super(imageURL, fileFormat, domain, fileName);
+    }
+    public static CommunityAttachment from(URL imageURL, FileFormat fileFormat, Domain domain,String fileName){
+        return new CommunityAttachment(imageURL, fileFormat, domain,fileName);
+    }
 }
--- src/main/java/com/devloop/attachment/entity/ProfileAttachment.java
+++ src/main/java/com/devloop/attachment/entity/ProfileAttachment.java
@@ -1,14 +1,24 @@
 package com.devloop.attachment.entity;
 
+import com.devloop.attachment.enums.Domain;
+import com.devloop.attachment.enums.FileFormat;
 import jakarta.persistence.DiscriminatorValue;
 import jakarta.persistence.Entity;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import java.net.URL;
 
 @Getter
 @NoArgsConstructor
 @Entity
 @DiscriminatorValue("F")
 public class ProfileAttachment extends Attachment {
+    private ProfileAttachment(URL imageURL, FileFormat fileFormat, Domain domain,  String fileName){
+        super(imageURL, fileFormat, domain,fileName);
+    }
 
+    public static ProfileAttachment from(URL imageURL, FileFormat fileFormat, Domain domain,String fileName){
+        return new ProfileAttachment(imageURL, fileFormat, domain,fileName);
+    }
 }
