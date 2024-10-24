f78735d36c633bbfaa9c9b96b6f0fdbd49a9f7e4
Ackrilll <skawlsgus2@naver.com>
2024-10-24 11:56:43 +0900

    [fix] uploadFile 메서드 UUID 적용된 파일 이름 리턴하도록 변경

--- src/main/java/com/devloop/common/utils/S3Util.java
+++ src/main/java/com/devloop/common/utils/S3Util.java
@@ -1,7 +1,11 @@
 package com.devloop.common.utils;
 
+import com.amazonaws.AmazonServiceException;
 import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.DeleteObjectRequest;
 import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.devloop.common.apipayload.status.ErrorStatus;
+import com.devloop.common.exception.ApiException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
@@ -25,7 +29,7 @@ public class S3Util {
 
     }
 
-    public void uploadFile(MultipartFile file){
+    public String uploadFile(MultipartFile file){
         String fileName = makeFileName(file);
         ObjectMetadata metadata = new ObjectMetadata();
         metadata.setContentType(file.getContentType());
@@ -33,13 +37,35 @@ public class S3Util {
 
         try {
             amazonS3Client.putObject(bucketName, fileName, file.getInputStream(), metadata);
+            return fileName;
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
 
+    public void delete(String fileName){
+        if(amazonS3Client.doesObjectExist(bucketName, fileName)) {
+            try {
+                amazonS3Client.deleteObject(new DeleteObjectRequest(bucketName,fileName));
+
+
+            } catch (AmazonServiceException e) {
+                throw new ApiException(ErrorStatus._HAS_NOT_ACCESS_PERMISSION);
+            }
+        } else {
+            throw new ApiException(ErrorStatus._ATTACHMENT_NOT_FOUND);
+        }
+    }
+
     // 버킷 이름과 오리지널 파일이름으로 URL 반환하는 메서드
-    public URL getUrl(String bucketName, String orginalFileName) {
+    public URL getUrl(String orginalFileName) {
         return amazonS3Client.getUrl(bucketName, orginalFileName);
+    }
+
+    public String extractFileNameFromS3Url(URL s3Url) {
+        String path = s3Url.getPath();
+        String[] parts = path.split("/");
+        return parts[parts.length - 1];
+
     }
 }
