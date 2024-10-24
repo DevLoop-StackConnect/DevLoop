fde4500720a83ddd2c2ef8b24d43aff91b1221f5
jiyumi <jiyumi00@naver.com>
2024-10-24 09:24:23 +0900

    merge:feat/login

--- src/main/java/com/devloop/party/request/SavePartyRequest.java
+++ src/main/java/com/devloop/party/request/SavePartyRequest.java
@@ -7,7 +7,6 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public class SavePartyRequest {
-
     @NotBlank(message = "제목을 작성해 주세요")
     private String title;
     @NotBlank(message = "내용을 작성해 주세요")
