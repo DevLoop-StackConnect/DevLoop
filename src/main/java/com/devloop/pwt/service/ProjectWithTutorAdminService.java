5677ffd5bb97450ac03dce874cf4ce93abb57471
kang-sumin <min001117@gmail.com>
2024-10-24 13:56:56 +0900

    [FEAT] 튜터랑 함께하는 협업 프로젝트 게시글 단건 조회(ADMIN : User 정보 포함 단건 조회)

--- src/main/java/com/devloop/pwt/service/ProjectWithTutorAdminService.java
+++ src/main/java/com/devloop/pwt/service/ProjectWithTutorAdminService.java
@@ -5,6 +5,7 @@ import com.devloop.common.enums.Approval;
 import com.devloop.common.exception.ApiException;
 import com.devloop.pwt.entity.ProjectWithTutor;
 import com.devloop.pwt.repository.ProjectWithTutorRepository;
+import com.devloop.pwt.response.ProjectWithTutorDetailAdminResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -29,4 +30,15 @@ public class ProjectWithTutorAdminService {
 
         return String.format("%s 게시글이 승인 되었습니다.", projectWithTutor.getTitle());
     }
+
+    // 튜터랑 함께하는 협업 프로젝트 게시글 단건 조회(ADMIN : User 정보 포함 단건 조회)
+    public ProjectWithTutorDetailAdminResponse getProjectWithTutor(Long projectId) {
+        // PWT 게시글 객체 가져오기
+        ProjectWithTutor projectWithTutor = projectWithTutorRepository.findById(projectId)
+                .orElseThrow(() -> new ApiException(ErrorStatus._NOT_FOUND_PROJECT_WITH_TUTOR));
+
+        return ProjectWithTutorDetailAdminResponse.from(projectWithTutor);
+    }
+
+
 }
