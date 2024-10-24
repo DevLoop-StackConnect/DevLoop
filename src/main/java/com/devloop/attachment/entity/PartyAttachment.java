8120aac8a75b8b71146b440c96ec9ef8bc39252b
Ackrilll <skawlsgus2@naver.com>
2024-10-24 11:55:51 +0900

    [Add] 자식 클래스 생성자 추가

--- src/main/java/com/devloop/attachment/entity/PartyAttachment.java
+++ src/main/java/com/devloop/attachment/entity/PartyAttachment.java
@@ -1,13 +1,23 @@
 package com.devloop.attachment.entity;
 
+import com.devloop.attachment.enums.Domain;
+import com.devloop.attachment.enums.FileFormat;
 import jakarta.persistence.DiscriminatorValue;
 import jakarta.persistence.Entity;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import java.net.URL;
 
 @Getter
 @NoArgsConstructor
 @Entity
 @DiscriminatorValue("P")
 public class PartyAttachment extends Attachment {
+    private PartyAttachment(URL imageURL, FileFormat fileFormat, Domain domain, String fileName){
+        super(imageURL, fileFormat, domain, fileName);
+    }
+    public static PartyAttachment from(URL imageURL, FileFormat fileFormat, Domain domain,String fileName){
+        return new PartyAttachment(imageURL, fileFormat, domain,fileName);
+    }
 }
