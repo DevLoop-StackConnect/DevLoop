6d6bcbd68685b219ddd8e5d3d49ce5a97a736388
Ackrilll <skawlsgus2@naver.com>
2024-10-25 12:57:42 +0900

    [refactor] SPQL 쿼리문 수정

--- src/main/java/com/devloop/community/repository/CommunityRepository.java
+++ src/main/java/com/devloop/community/repository/CommunityRepository.java
@@ -4,10 +4,11 @@ import com.devloop.community.entity.Community;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
 public interface CommunityRepository extends JpaRepository<Community,Long>, JpaSpecificationExecutor<Community> {
-    @Query("select Community from Community c where User.id =:userId")
-    Optional<Community> findByUserId(Long userId);
+    @Query("select Community from Community c where c.user.id =:userId")
+    Optional<Community> findByUserId(@Param("userId") Long userId);
 }
