ca88c2a489a03bce661688ce0f0a55443e4c3108
Ackrilll <skawlsgus2@naver.com>
2024-10-25 13:01:04 +0900

    [refactor] @Param 붙여줌

--- src/main/java/com/devloop/tutor/repository/TutorRequestRepository.java
+++ src/main/java/com/devloop/tutor/repository/TutorRequestRepository.java
@@ -21,5 +21,5 @@ public interface TutorRequestRepository extends JpaRepository<TutorRequest, Long
             Pageable pageable,
             @Param("status") TutorRequestStatus tutorRequestStatus);
 
-    Optional<TutorRequest> findByUserId(Long userId);
+    Optional<TutorRequest> findByUserId(@Param("userId") Long userId);
 }
