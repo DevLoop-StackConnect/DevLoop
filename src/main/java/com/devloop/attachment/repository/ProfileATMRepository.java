1b129e2c6db8887165daf26b79bd54481f546c44
Ackrilll <skawlsgus2@naver.com>
2024-10-25 16:38:24 +0900

    [Refactor] 경로 삭제

--- src/main/java/com/devloop/attachment/repository/ProfileATMRepository.java
+++ src/main/java/com/devloop/attachment/repository/ProfileATMRepository.java
@@ -1,6 +1,7 @@
 package com.devloop.attachment.repository;
 
+import com.devloop.attachment.entity.ProfileAttachment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ProfileATMRepository extends JpaRepository<com.devloop.attachment.entity.ProfileAttachment, Long> {
+public interface ProfileATMRepository extends JpaRepository<ProfileAttachment, Long> {
 }
