d5cf0f87696189b32c8eef348363f409312a4df2
Ackrilll <skawlsgus2@naver.com>
2024-10-25 09:53:28 +0900

    [refactor] 각 도메인 첨부파일 엔티티 이름 변경

--- src/main/java/com/devloop/attachment/repository/PartyAMTRepository.java
+++ src/main/java/com/devloop/attachment/repository/PartyAMTRepository.java
@@ -3,5 +3,5 @@ package com.devloop.attachment.repository;
 import com.devloop.attachment.entity.PartyAttachment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PARepository extends JpaRepository<PartyAttachment, Long> {
+public interface PartyAMTRepository extends JpaRepository<PartyAttachment, Long> {
 }
