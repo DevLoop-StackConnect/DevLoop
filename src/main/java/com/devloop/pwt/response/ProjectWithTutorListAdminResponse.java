b7b70199e4ca3202b5550007ca7ae95a957a40f0
kang-sumin <min001117@gmail.com>
2024-10-25 11:57:09 +0900

    [REFACTOR] ProjectWithTutorListAdminResponse.java of메서드로 리팩토링

--- src/main/java/com/devloop/pwt/response/ProjectWithTutorListAdminResponse.java
+++ src/main/java/com/devloop/pwt/response/ProjectWithTutorListAdminResponse.java
@@ -1,16 +1,10 @@
 package com.devloop.pwt.response;
 
-import com.devloop.common.apipayload.dto.ProjectWithTutorResponseDto;
-import com.devloop.pwt.enums.Level;
-import com.devloop.pwt.enums.ProjectWithTutorStatus;
-import com.devloop.user.entity.User;
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 
 import java.time.LocalDateTime;
 
 @Getter
-@RequiredArgsConstructor
 public class ProjectWithTutorListAdminResponse {
 
     private final Long id;
@@ -26,34 +20,41 @@ public class ProjectWithTutorListAdminResponse {
             Long id,
             String title,
             Integer price,
-            ProjectWithTutorStatus status,
+            String status,
             LocalDateTime deadline,
             Integer maxParticipants,
-            Level level,
-            User tutorName
+            String level,
+            String tutorName
     ) {
         this.id = id;
         this.title = title;
         this.price = price;
-        this.status = status.getStatus();
+        this.status = status;
         this.deadline = deadline;
         this.maxParticipants = maxParticipants;
-        this.level = level.getLevel();
-        this.tutorName = tutorName.getUsername();
+        this.level = level;
+        this.tutorName = tutorName;
     }
 
-    public static ProjectWithTutorListAdminResponse from(
-            ProjectWithTutorResponseDto project
+    public static ProjectWithTutorListAdminResponse of(
+            Long id,
+            String title,
+            Integer price,
+            String status,
+            LocalDateTime deadline,
+            Integer maxParticipants,
+            String level,
+            String tutorName
     ) {
         return new ProjectWithTutorListAdminResponse(
-                project.getId(),
-                project.getTitle(),
-                project.getPrice(),
-                project.getStatus(),
-                project.getDeadline(),
-                project.getMaxParticipants(),
-                project.getLevel(),
-                project.getUser()
+                id,
+                title,
+                price,
+                status,
+                deadline,
+                maxParticipants,
+                level,
+                tutorName
         );
     }
 }
