5dd6152f4144d2a4aaf396d36ce8d9a6cd26e439
Ackrilll <skawlsgus2@naver.com>
2024-10-26 14:45:44 +0900

    [Refactor] 프로필 이미지 여러장 들어오지 못하게 수정

--- src/main/java/com/devloop/user/controller/UserController.java
+++ src/main/java/com/devloop/user/controller/UserController.java
@@ -24,8 +24,8 @@ public class UserController {
     }
 
     @PatchMapping("/v1/users/profiles/image")
-    public ApiResponse<String>updateProfileImg(@RequestParam("file") MultipartFile file, @AuthenticationPrincipal AuthUser authUser){
-        userService.updateProfileImg(file, authUser);
+    public ApiResponse<String>updateProfileImg(@RequestParam("file") MultipartFile[] files, @AuthenticationPrincipal AuthUser authUser){
+        userService.updateProfileImg(files, authUser);
         return ApiResponse.ok("프로필 이미지가 변경 되었습니다.");
     }
 }
