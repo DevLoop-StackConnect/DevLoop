7694d8e9cd946e8f480fe05c1083e21ea55071b4
macbookpro <ks12467@naver.com>
2024-10-26 18:26:55 +0900

    [TEST] Auth Service TestCode Add

--- src/main/java/com/devloop/auth/request/SignoutRequest.java
+++ src/main/java/com/devloop/auth/request/SignoutRequest.java
@@ -1,7 +1,11 @@
 package com.devloop.auth.request;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
 
+@AllArgsConstructor
 @Getter
 public class SignoutRequest {
     private String password;
--- src/main/java/com/devloop/auth/response/SignupResponse.java
+++ src/main/java/com/devloop/auth/response/SignupResponse.java
@@ -7,18 +7,20 @@ import java.time.LocalDateTime;
 @Getter
 public class SignupResponse {
 
+    private final Long id;
     private final String email;
     private final String name;
     private final LocalDateTime createdAt;
 
-    private SignupResponse(String email, String name, LocalDateTime createdAt) {
+    private SignupResponse(Long id, String email, String name, LocalDateTime createdAt) {
+        this.id = id;
         this.email = email;
         this.name = name;
         this.createdAt = createdAt;
     }
 
-    public static SignupResponse of(String email, String name, LocalDateTime createdAt) {
-        return new SignupResponse(email, name, createdAt);
+    public static SignupResponse of(Long id, String email, String name, LocalDateTime createdAt) {
+        return new SignupResponse(id ,email, name, createdAt);
     }
 }
 
