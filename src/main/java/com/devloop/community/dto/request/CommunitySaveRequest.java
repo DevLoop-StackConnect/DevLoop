fb53cd4535b7824b410e6857321cb7c244d14242
goodsoundisme <sol000619@naver.com>
2024-10-27 13:43:26 +0900

    [FEAT] 첨부파일기능

--- src/main/java/com/devloop/community/dto/request/CommunitySaveRequest.java
+++ src/main/java/com/devloop/community/dto/request/CommunitySaveRequest.java
@@ -1,6 +1,5 @@
 package com.devloop.community.dto.request;
 
-import com.devloop.common.enums.Category;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
