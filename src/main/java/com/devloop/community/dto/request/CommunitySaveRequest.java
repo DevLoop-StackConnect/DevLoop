7f2c748dc9a02a22e5ad4bba5249bb1c45dc261c
goodsoundisme <sol000619@naver.com>
2024-10-27 14:46:58 +0900

    [REFACTOR] NoArgsConstructor 제거

--- src/main/java/com/devloop/community/dto/request/CommunitySaveRequest.java
+++ src/main/java/com/devloop/community/dto/request/CommunitySaveRequest.java
@@ -4,11 +4,9 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 
 @Getter
 @AllArgsConstructor
-@RequiredArgsConstructor
 public class CommunitySaveRequest {
     @NotBlank(message = "제목을 작성해 주세요")
     private String title;
