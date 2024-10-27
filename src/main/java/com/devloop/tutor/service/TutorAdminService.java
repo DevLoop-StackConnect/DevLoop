23f6415454b9a3c43b27c8c252c6aad074059155
kang-sumin <min001117@gmail.com>
2024-10-27 19:28:52 +0900

    [REFACTOR] UserService 주입 방식으로 리팩토링

--- src/main/java/com/devloop/tutor/service/TutorAdminService.java
+++ src/main/java/com/devloop/tutor/service/TutorAdminService.java
@@ -9,6 +9,7 @@ import com.devloop.tutor.response.TutorRequestListAdminResponse;
 import com.devloop.user.entity.User;
 import com.devloop.user.enums.UserRole;
 import com.devloop.user.repository.UserRepository;
+import com.devloop.user.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -22,8 +23,7 @@ import org.springframework.transaction.annotation.Transactional;
 public class TutorAdminService {
 
     private final TutorRequestRepository requestRepository;
-    // todo : UserService 주입받는 방식으로 리팩토링 하기
-    private final UserRepository userRepository;
+    private final UserService userService;
 
     // 튜터 신청 요청 조회 (ADMIN : 승인되지 않은 튜터 신청 요청 다건 조회)
     public Page<TutorRequestListAdminResponse> getAllTutorRequest(int page, int size) {
@@ -41,8 +41,7 @@ public class TutorAdminService {
     @Transactional
     public String changeUserRoleToTutor(Long userId) {
         // 사용자 객체 가져오기
-        User user = userRepository.findById(userId)
-                .orElseThrow(() -> new ApiException(ErrorStatus._NOT_FOUND_USER));
+        User user = userService.findByUserId(userId);
 
         // 튜터 신청 내역 가져오기
         TutorRequest tutorRequest = requestRepository.findByUserId(userId)
