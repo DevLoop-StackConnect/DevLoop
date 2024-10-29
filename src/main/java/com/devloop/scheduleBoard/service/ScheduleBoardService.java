337a99f05d5d998ce5ed471b768be01d60e7c631
goodsoundisme <sol000619@naver.com>
2024-10-29 20:48:36 +0900

    [REFACTOR] 스케줄 보드에서 todo리스트 보이게 하지 않고, todo서비스에서 다건 조회로 볼 수 있도록 수정

--- src/main/java/com/devloop/scheduleBoard/service/ScheduleBoardService.java
+++ src/main/java/com/devloop/scheduleBoard/service/ScheduleBoardService.java
@@ -3,55 +3,49 @@ package com.devloop.scheduleBoard.service;
 import com.devloop.common.apipayload.status.ErrorStatus;
 import com.devloop.common.exception.ApiException;
 import com.devloop.pwt.entity.ProjectWithTutor;
-import com.devloop.pwt.repository.ProjectWithTutorRepository;
+import com.devloop.pwt.service.ProjectWithTutorService;
 import com.devloop.scheduleBoard.dto.response.ScheduleBoardResponse;
 import com.devloop.scheduleBoard.entity.ScheduleBoard;
 import com.devloop.scheduleBoard.repository.ScheduleBoardRepository;
-import com.devloop.scheduleTodo.dto.response.ScheduleTodoSimpleResponse;
-import com.devloop.scheduleTodo.repository.ScheduleTodoRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.List;
-import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
 public class ScheduleBoardService {
     private final ScheduleBoardRepository scheduleBoardRepository;
-    private final ProjectWithTutorRepository projectWithTutorRepository;
-    private final ScheduleTodoRepository scheduleTodoRepository;
+    private final ProjectWithTutorService projectWithTutorService;
+
 
     //PWT 승인 시 스케줄보드 생성 메서드
     @Transactional
-    public ScheduleBoard createScheduleBoard(ProjectWithTutor projectWithTutor){
+    public ScheduleBoard createScheduleBoard(ProjectWithTutor projectWithTutor) {
         //스케줄보드객체 생성해서 db에 저장
-        ScheduleBoard scheduleBoard = ScheduleBoard.of(projectWithTutor,projectWithTutor.getUser());
+        ScheduleBoard scheduleBoard = ScheduleBoard.of(projectWithTutor, projectWithTutor.getUser());
         return scheduleBoardRepository.save(scheduleBoard);
     }
 
     //스케줄 보드 조회
     @Transactional
     public ScheduleBoardResponse getScheduleBoard(Long pwtId) {
-        ProjectWithTutor projectWithTutor = projectWithTutorRepository.findById(pwtId)
-                .orElseThrow(()-> new ApiException(ErrorStatus._NOT_FOUND_PROJECT_WITH_TUTOR));
+        ProjectWithTutor projectWithTutor = projectWithTutorService.findByPwtId(pwtId);
 
         ScheduleBoard scheduleBoard = scheduleBoardRepository.findByProjectWithTutor(projectWithTutor)
-                .orElseThrow(()->new ApiException(ErrorStatus._NOT_FOUND_SCHEDULE_BOARD));
+                .orElseThrow(() -> new ApiException(ErrorStatus._NOT_FOUND_SCHEDULE_BOARD));
 
-        //SceduleTodo 목록 조회
-        List<ScheduleTodoSimpleResponse> todos = scheduleTodoRepository.findByScheduleBoard(scheduleBoard)
-                .stream()
-                .map(todo->ScheduleTodoSimpleResponse.of(todo.getTitle(),todo.getStartDate(),todo.getEndDate()))
-                .collect(Collectors.toList());
 
         return ScheduleBoardResponse.of(
                 scheduleBoard.getId(),
                 projectWithTutor.getId(),
-                scheduleBoard.getManagerTutor().getUsername(),
-                todos
+                scheduleBoard.getManagerTutor().getUsername()
         );
+    }
+
+    //util
+    public ScheduleBoard findByScheduleBoardId(Long scheduleBoardId) {
+        return scheduleBoardRepository.findById(scheduleBoardId)
+                .orElseThrow(() -> new ApiException(ErrorStatus._NOT_FOUND_SCHEDULE_BOARD));
     }
 }
