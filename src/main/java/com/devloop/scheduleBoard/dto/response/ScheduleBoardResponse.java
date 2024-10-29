b6b03436bbceb12fe674405aa6be46b492c5ca60
goodsoundisme <sol000619@naver.com>
2024-10-29 20:48:36 +0900

    [REFACTOR] 스케줄 보드에서 todo리스트 보이게 하지 않고, todo서비스에서 다건 조회로 볼 수 있도록 수정

--- src/main/java/com/devloop/scheduleBoard/dto/response/ScheduleBoardResponse.java
+++ src/main/java/com/devloop/scheduleBoard/dto/response/ScheduleBoardResponse.java
@@ -10,21 +10,18 @@ public class ScheduleBoardResponse {
     private final Long id;
     private final Long pwtId;
     private final String managerTutorName;
-    private final List<ScheduleTodoSimpleResponse> todos;
 
-    private ScheduleBoardResponse(Long id, Long pwtId, String managerTutorName, List<ScheduleTodoSimpleResponse> todos) {
+    private ScheduleBoardResponse(Long id, Long pwtId, String managerTutorName) {
         this.id = id;
         this.pwtId = pwtId;
         this.managerTutorName = managerTutorName;
-        this.todos=todos;
     }
 
-    public static ScheduleBoardResponse of(Long id, Long pwtId, String managerTutorName,List<ScheduleTodoSimpleResponse> todos){
+    public static ScheduleBoardResponse of(Long id, Long pwtId, String managerTutorName){
         return new ScheduleBoardResponse(
                 id,
                 pwtId,
-                managerTutorName,
-                todos
+                managerTutorName
         );
     }
 }
