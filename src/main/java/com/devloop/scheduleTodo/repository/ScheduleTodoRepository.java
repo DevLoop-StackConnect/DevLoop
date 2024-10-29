bdd455d71c04445dbf72680da25ee7bf136f12a2
goodsoundisme <sol000619@naver.com>
2024-10-29 15:52:38 +0900

    [FEAT] 스케줄보드에서 todos 다건조회 기능

--- src/main/java/com/devloop/scheduleTodo/repository/ScheduleTodoRepository.java
+++ src/main/java/com/devloop/scheduleTodo/repository/ScheduleTodoRepository.java
@@ -1,7 +1,12 @@
 package com.devloop.scheduleTodo.repository;
 
+import com.devloop.scheduleBoard.entity.ScheduleBoard;
+import com.devloop.scheduleTodo.dto.response.ScheduleTodoSimpleResponse;
 import com.devloop.scheduleTodo.entity.ScheduleTodo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ScheduleTodoRepository extends JpaRepository<ScheduleTodo,Long> {
+    List<ScheduleTodo> findByScheduleBoard(ScheduleBoard scheduleBoard);
 }
