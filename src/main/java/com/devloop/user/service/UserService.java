003f3a7ab3bdcf14f1cbc57c918a43ef58e31ce7
Ackrilll <skawlsgus2@naver.com>
2024-11-01 15:05:54 +0900

    [Remove] s3테스트 api 삭제

--- src/main/java/com/devloop/user/service/UserService.java
+++ src/main/java/com/devloop/user/service/UserService.java
@@ -101,16 +101,7 @@ public class UserService {
         s3Service.uploadFile(file,user,user);
     }
 
-    public S3Object getProfileImgFromS3(AuthUser authUser) {
-        User user = userRepository.findById(authUser.getId())
-                .orElseThrow(()->new ApiException(ErrorStatus._NOT_FOUND_USER));
 
-        ProfileAttachment profileAttachment = profileATMRepository.findById(user.getAttachmentId())
-                .orElseThrow(()->new ApiException(ErrorStatus._ATTACHMENT_NOT_FOUND));
-        String fileName = profileAttachment.getFileName();
-
-        return s3Service.getProfileImgFromS3(fileName);
-    }
     //----------------------------------------------------util---------------------------------------------------//
     public User findByUserId(Long userId) {
         return userRepository.findById(userId).orElseThrow(()->new ApiException(ErrorStatus._NOT_FOUND_USER));
