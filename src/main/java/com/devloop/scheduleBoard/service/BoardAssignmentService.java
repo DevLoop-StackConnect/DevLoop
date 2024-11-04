7ba881048855788a1f1c4e4f54c0c1a94ac0bbd7
goodsoundisme <sol000619@naver.com>
2024-11-04 21:16:34 +0900

    [REFACTOR] purchase테이블과 boardAssignment 연관관계 맺기

---
+++ src/main/java/com/devloop/scheduleBoard/service/BoardAssignmentService.java
@@ -0,0 +1,40 @@
+package com.devloop.scheduleBoard.service;
+
+import com.devloop.purchase.entity.Purchase;
+import com.devloop.pwt.entity.ProjectWithTutor;
+import com.devloop.scheduleBoard.entity.BoardAssignment;
+import com.devloop.scheduleBoard.entity.ScheduleBoard;
+import com.devloop.scheduleBoard.repository.BoardAssignmentRepository;
+import lombok.RequiredArgsConstructor;
+import org.hibernate.proxy.HibernateProxy;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class BoardAssignmentService {
+    private final BoardAssignmentRepository boardAssignmentRepository;
+
+    //BoardAssignment 생성 메서드
+    @Transactional
+    public void createBoardAssignment(List<Purchase> purchases) {
+       for (Purchase purchase : purchases) {
+           if (((HibernateProxy)purchase.getProduct()).getHibernateLazyInitializer().getImplementationClass()== ProjectWithTutor.class){
+
+               // ProjectWithTutor로 실제 객체를 가져오기
+               ProjectWithTutor projectWithTutor = (ProjectWithTutor) ((HibernateProxy) purchase.getProduct()).getHibernateLazyInitializer().getImplementation();
+               ScheduleBoard scheduleBoard = projectWithTutor.getScheduleBoard();
+
+               // ScheduleBoard가 있는 경우에만 BoardAssignment 생성
+               if (scheduleBoard != null) {
+                   BoardAssignment boardAssignment = BoardAssignment.of(scheduleBoard, purchase);
+                   boardAssignmentRepository.save(boardAssignment);
+               }
+           }
+       }
+    }
+}
+//           purchase.getProduct().
