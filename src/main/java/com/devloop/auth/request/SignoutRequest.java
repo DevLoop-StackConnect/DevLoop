eee30510f32508a576457b08fddeaf35711e7698
macbookpro <ks12467@naver.com>
2024-11-05 21:16:25 +0900

    [Refectoring] Auth Request NotBlank로 수정

--- src/main/java/com/devloop/auth/request/SignoutRequest.java
+++ src/main/java/com/devloop/auth/request/SignoutRequest.java
@@ -1,11 +1,11 @@
 package com.devloop.auth.request;
 
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 
 @Getter
 public class SignoutRequest {
-    @NotNull(message = "비밀번호를 입력해주세요.")
+    @NotBlank(message = "비밀번호를 입력해주세요.")
 //    @Pattern(regexp = "^(?=.*?[A-Za-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]).{8,}$",
 //            message = "대소문자 포함 영문 + 숫자 + 특수문자를 최소 1글자씩 포함하여 최소 8글자 이상이어야 합니다.")
     private String password;
