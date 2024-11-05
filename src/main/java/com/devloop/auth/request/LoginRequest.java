319b69d0b0980f6fe78f6fa4f65433ff6c67f31b
macbookpro <ks12467@naver.com>
2024-11-05 21:23:46 +0900

    [Refectoring] Auth Request NotBlank로 수정

--- src/main/java/com/devloop/auth/request/LoginRequest.java
+++ src/main/java/com/devloop/auth/request/LoginRequest.java
@@ -2,11 +2,12 @@ package com.devloop.auth.request;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import lombok.AccessLevel;
 import lombok.Getter;
-
-
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class LoginRequest {
 
     @NotBlank(message = "이메일을 입력해주세요")
--- src/main/java/com/devloop/auth/request/SignupRequest.java
+++ src/main/java/com/devloop/auth/request/SignupRequest.java
@@ -3,9 +3,12 @@ package com.devloop.auth.request;
 import com.devloop.user.enums.UserRole;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class SignupRequest {
 
     @NotBlank(message = "이메일은 필수 입력사항입니다.")
