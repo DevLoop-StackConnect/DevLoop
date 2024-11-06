d02ab11ec96755730facff709542b7da638f6ac6
macbookpro <ks12467@naver.com>
2024-11-06 20:14:17 +0900

    [Refectoring] Party Comment Refectoring /request AccessLevel add

--- src/main/java/com/devloop/partycomment/response/SavePartyCommentResponse.java
+++ src/main/java/com/devloop/partycomment/response/SavePartyCommentResponse.java
@@ -8,13 +8,13 @@ public class SavePartyCommentResponse {
     private final Long commentId;
     private final String comment;
 
-    private SavePartyCommentResponse(Long partyId,Long commentId,String comment){
-        this.partyId=partyId;
-        this.commentId=commentId;
-        this.comment=comment;
+    private SavePartyCommentResponse(Long partyId, Long commentId, String comment) {
+        this.partyId = partyId;
+        this.commentId = commentId;
+        this.comment = comment;
     }
 
-    public static SavePartyCommentResponse of(Long partyId,Long commentId,String comment){
+    public static SavePartyCommentResponse of(Long partyId, Long commentId, String comment) {
         return new SavePartyCommentResponse(
                 partyId,
                 commentId,
