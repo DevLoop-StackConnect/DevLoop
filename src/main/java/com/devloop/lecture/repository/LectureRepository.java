2188b98ab2faf9b4edd9818b4ddd692ce60b395d
macbookpro <ks12467@naver.com>
2024-11-07 06:14:25 +0900

    [Update] Search Lecture Add

--- src/main/java/com/devloop/lecture/repository/LectureRepository.java
+++ src/main/java/com/devloop/lecture/repository/LectureRepository.java
@@ -5,8 +5,9 @@ import com.devloop.lecture.entity.Lecture;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-public interface LectureRepository extends JpaRepository<Lecture,Long> {
+public interface LectureRepository extends JpaRepository<Lecture,Long> , JpaSpecificationExecutor<Lecture> {
     Page<Lecture> findByTitleContainingAndApproval(String title, Approval approval,PageRequest pageable);
 
     Page<Lecture> findByApproval(Approval approval, PageRequest pageable);
