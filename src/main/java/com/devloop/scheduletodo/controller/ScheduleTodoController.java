46f2c1102ea3e858ba052878f4aa38a812f66080
Ackrilll <skawlsgus2@naver.com>
2024-11-06 21:10:28 +0900

    [Refactor] ScheduleTodo 리펙토링

--- src/main/java/com/devloop/scheduletodo/controller/ScheduleTodoController.java
+++ src/main/java/com/devloop/scheduletodo/controller/ScheduleTodoController.java
@@ -8,6 +8,8 @@ import com.devloop.scheduletodo.response.ScheduleTodoSimpleResponse;
 import com.devloop.scheduletodo.service.ScheduleTodoService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
@@ -22,33 +24,39 @@ public class ScheduleTodoController {
 
     //일정 생성
     @PostMapping
-    public ApiResponse<ScheduleTodoResponse> createScheduleTodo(@AuthenticationPrincipal AuthUser authUser, @PathVariable Long scheduleBoardId, @RequestBody @Valid ScheduleTodoRequest scheduleTodoRequest) {
+    public ApiResponse<ScheduleTodoResponse> createScheduleTodo(@AuthenticationPrincipal AuthUser authUser,
+                                                                @PathVariable Long scheduleBoardId,
+                                                                @RequestBody @Valid ScheduleTodoRequest scheduleTodoRequest) {
         return ApiResponse.ok(scheduleTodoService.createScheduleTodo(scheduleBoardId, scheduleTodoRequest, authUser));
     }
 
     //일정 다건 조회
     @GetMapping
+    @PreAuthorize("permitAll()")
     public ApiResponse<List<ScheduleTodoSimpleResponse>> getTodoByScheduleBoard(@PathVariable Long scheduleBoardId) {
         return ApiResponse.ok(scheduleTodoService.getTodoByScheduleBoard(scheduleBoardId));
     }
 
     //일정 단건 조회
     @GetMapping("/{scheduleTodoId}")
+    @PreAuthorize("permitAll()")
     public ApiResponse<ScheduleTodoResponse> getScheduleTodo(@PathVariable Long scheduleTodoId) {
         return ApiResponse.ok(scheduleTodoService.getScheduleTodo(scheduleTodoId));
     }
 
     //일정 수정
     @PatchMapping("/{scheduleTodoId}")
-    public ApiResponse<ScheduleTodoResponse> updateScheduleTodo(@AuthenticationPrincipal AuthUser authUser, @PathVariable Long scheduleTodoId, @RequestBody ScheduleTodoRequest scheduleTodoRequest) {
+    public ApiResponse<ScheduleTodoResponse> updateScheduleTodo(@AuthenticationPrincipal AuthUser authUser,
+                                                                @PathVariable Long scheduleTodoId,
+                                                                @RequestBody @Valid ScheduleTodoRequest scheduleTodoRequest) {
         return ApiResponse.ok(scheduleTodoService.updateScheduleTodo(authUser, scheduleTodoId, scheduleTodoRequest));
     }
 
     //일정 삭제
     @DeleteMapping("/{scheduleTodoId}")
-    public ApiResponse<String> deleteScheduleTodo(@PathVariable Long scheduleTodoId, @AuthenticationPrincipal AuthUser authUser) {
+    public ResponseEntity<Void> deleteScheduleTodo(@PathVariable Long scheduleTodoId, @AuthenticationPrincipal AuthUser authUser) {
         scheduleTodoService.deleteScheduleTodo(scheduleTodoId, authUser);
-        return ApiResponse.ok(null);
+        return ResponseEntity.noContent().build();
 
     }
 
