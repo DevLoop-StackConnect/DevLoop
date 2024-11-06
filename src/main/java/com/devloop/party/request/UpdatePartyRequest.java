c645341a32061553759650db021d98fcef9ba9fa
macbookpro <ks12467@naver.com>
2024-11-06 19:55:23 +0900

    [Refectoring] Party Refectoring /request AccessLevel add

--- src/main/java/com/devloop/party/request/UpdatePartyRequest.java
+++ src/main/java/com/devloop/party/request/UpdatePartyRequest.java
@@ -2,11 +2,14 @@ package com.devloop.party.request;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
-@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
 public class UpdatePartyRequest {
     @NotBlank(message = "제목을 작성해 주세요")
     private String title;
