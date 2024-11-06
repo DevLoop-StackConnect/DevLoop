bfd44ad15e538e317878e5fd7f4d6894b48865a0
Ackrilll <skawlsgus2@naver.com>
2024-11-06 16:39:48 +0900

    [Refactor]

--- src/main/java/com/devloop/scheduleboard/entity/BoardAssignment.java
+++ src/main/java/com/devloop/scheduleboard/entity/BoardAssignment.java
@@ -19,17 +19,17 @@ public class BoardAssignment {
     private ScheduleBoard scheduleBoard;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name="purchase_id",nullable = false)
+    @JoinColumn(name = "purchase_id", nullable = false)
     private Purchase purchase;
 
-    private BoardAssignment(ScheduleBoard scheduleBoard, Purchase purchase){
-        this.scheduleBoard=scheduleBoard;
-        this.purchase=purchase;
+    private BoardAssignment(ScheduleBoard scheduleBoard, Purchase purchase) {
+        this.scheduleBoard = scheduleBoard;
+        this.purchase = purchase;
     }
 
     public static BoardAssignment of(
             ScheduleBoard scheduleBoard,
             Purchase purchase) {
-        return new BoardAssignment(scheduleBoard,purchase);
+        return new BoardAssignment(scheduleBoard, purchase);
     }
 }
