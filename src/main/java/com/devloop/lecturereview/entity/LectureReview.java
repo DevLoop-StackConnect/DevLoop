b2fbd2b8c341d1f58c5b9f32370e07c42245d0dc
jiyumi <jiyumi00@naver.com>
2024-11-06 20:17:57 +0900

    refactor: 댓글 리팩토링

--- src/main/java/com/devloop/lecturereview/entity/LectureReview.java
+++ src/main/java/com/devloop/lecturereview/entity/LectureReview.java
@@ -5,7 +5,6 @@ import com.devloop.lecture.entity.Lecture;
 import com.devloop.lecturereview.request.SaveLectureReviewRequest;
 import com.devloop.user.entity.User;
 import jakarta.persistence.*;
-import jakarta.validation.constraints.NotNull;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -21,25 +20,25 @@ public class LectureReview extends Timestamped {
     @Column(length = 255, nullable = false)
     private String review;
 
-    @NotNull
+    @Column(nullable = false)
     private Integer rating;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name="user_id")
+    @JoinColumn(name = "user_id", nullable = false)
     private User user;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name="lecture_id")
+    @JoinColumn(name = "lecture_id", nullable = false)
     private Lecture lecture;
 
-    private LectureReview(String review,Integer rating,User user,Lecture lecture){
-        this.review=review;
-        this.rating=rating;
-        this.user=user;
-        this.lecture=lecture;
+    private LectureReview(String review, Integer rating, User user, Lecture lecture) {
+        this.review = review;
+        this.rating = rating;
+        this.user = user;
+        this.lecture = lecture;
     }
 
-    public static LectureReview from(SaveLectureReviewRequest request, User user, Lecture lecture){
+    public static LectureReview from(SaveLectureReviewRequest request, User user, Lecture lecture) {
         return new LectureReview(
                 request.getReview(),
                 request.getRating(),
@@ -48,8 +47,8 @@ public class LectureReview extends Timestamped {
         );
     }
 
-    public void update(SaveLectureReviewRequest request){
-        this.review=request.getReview();
-        this.rating=request.getRating();
+    public void update(SaveLectureReviewRequest request) {
+        this.review = request.getReview();
+        this.rating = request.getRating();
     }
 }
