bfd9aaa6ffade6f74e18637ff58a021f86a8e226
kang-sumin <min001117@gmail.com>
2024-11-06 17:13:25 +0900

    [REFACTOR] payment 도메인 리팩토링

--- src/main/java/com/devloop/payment/controller/PaymentController.java
+++ src/main/java/com/devloop/payment/controller/PaymentController.java
@@ -4,7 +4,6 @@ import com.devloop.order.entity.Order;
 import com.devloop.order.service.OrderService;
 import com.devloop.payment.service.PaymentService;
 import com.devloop.purchase.service.PurchaseService;
-import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
@@ -13,17 +12,20 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
-import java.io.*;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.Reader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
 import java.util.Base64;
-import java.util.UUID;
 
-//@RequestMapping(value="/")
 @Controller
 @RequiredArgsConstructor
 public class PaymentController {
@@ -75,7 +77,7 @@ public class PaymentController {
         return "payment-fail";
     }
 
-
+    // 결제 승인 요청
     @RequestMapping("/confirm")
     public ResponseEntity<JSONObject> confirmPayment(@RequestBody String jsonBody) throws Exception {
 
@@ -92,7 +94,7 @@ public class PaymentController {
         } catch (ParseException e) {
             throw new RuntimeException(e);
         }
-        ;
+
         JSONObject obj = new JSONObject();
         obj.put("orderId", orderId);
         obj.put("amount", amount);
@@ -100,7 +102,6 @@ public class PaymentController {
 
         // 토스페이먼츠 API는 시크릿 키를 사용자 ID로 사용하고, 비밀번호는 사용하지 않습니다.
         // 비밀번호가 없다는 것을 알리기 위해 시크릿 키 뒤에 콜론을 추가합니다.
-
         Base64.Encoder encoder = Base64.getEncoder();
         byte[] encodedBytes = encoder.encode((widgetSecretKey + ":").getBytes(StandardCharsets.UTF_8));
         String authorizations = "Basic " + new String(encodedBytes);
@@ -127,7 +128,7 @@ public class PaymentController {
         responseStream.close();
 
         // 주문 객체 상태 변경 , 구매내역, 줌문 내역 객체 생성
-        if(isSuccess){
+        if (isSuccess) {
             // 구매 내역 생성
             purchaseService.createPurchase(jsonObject.get("orderId").toString());
             // 주문 상태 완료로 변경
@@ -138,79 +139,5 @@ public class PaymentController {
 
         return ResponseEntity.status(code).body(jsonObject);
     }
-
-//    @GetMapping(value = "success")
-//    public String paymentResult(
-//            Model model,
-//            @RequestParam(value = "orderId") String orderId,
-//            @RequestParam(value = "amount") Integer amount,
-//            @RequestParam(value = "paymentKey") String paymentKey) throws Exception {
-//
-//        String secretKey = "시크릿 키 전역변수로 설정되어 있는것 사용";
-//
-//        Base64.Encoder encoder = Base64.getEncoder();
-//        byte[] encodedBytes = encoder.encode(secretKey.getBytes("UTF-8"));
-//        String authorizations = "Basic " + new String(encodedBytes, 0, encodedBytes.length);
-//
-//        URL url = new URL("https://api.tosspayments.com/v1/payments/" + paymentKey);
-//
-//        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-//        connection.setRequestProperty("Authorization", authorizations);
-//        connection.setRequestProperty("Content-Type", "application/json");
-//        connection.setRequestMethod("POST");
-//        connection.setDoOutput(true);
-//        JSONObject obj = new JSONObject();
-//        obj.put("orderId", orderId);
-//        obj.put("amount", amount);
-//
-//        OutputStream outputStream = connection.getOutputStream();
-//        outputStream.write(obj.toString().getBytes("UTF-8"));
-//
-//        int code = connection.getResponseCode();
-//        boolean isSuccess = code == 200 ? true : false;
-//        model.addAttribute("isSuccess", isSuccess);
-//
-//        InputStream responseStream = isSuccess ? connection.getInputStream() : connection.getErrorStream();
-//
-//        Reader reader = new InputStreamReader(responseStream, StandardCharsets.UTF_8);
-//        JSONParser parser = new JSONParser();
-//        JSONObject jsonObject = (JSONObject) parser.parse(reader);
-//        responseStream.close();
-//        model.addAttribute("responseStr", jsonObject.toJSONString());
-//        System.out.println(jsonObject.toJSONString());
-//
-//        model.addAttribute("method", (String) jsonObject.get("method"));
-//        model.addAttribute("orderName", (String) jsonObject.get("orderName"));
-//
-//        if (((String) jsonObject.get("method")) != null) {
-//            if (((String) jsonObject.get("method")).equals("카드")) {
-//                model.addAttribute("cardNumber", (String) ((JSONObject) jsonObject.get("card")).get("number"));
-//            } else if (((String) jsonObject.get("method")).equals("가상계좌")) {
-//                model.addAttribute("accountNumber", (String) ((JSONObject) jsonObject.get("virtualAccount")).get("accountNumber"));
-//            } else if (((String) jsonObject.get("method")).equals("계좌이체")) {
-//                model.addAttribute("bank", (String) ((JSONObject) jsonObject.get("transfer")).get("bank"));
-//            } else if (((String) jsonObject.get("method")).equals("휴대폰")) {
-//                model.addAttribute("customerMobilePhone", (String) ((JSONObject) jsonObject.get("mobilePhone")).get("customerMobilePhone"));
-//            }
-//        } else {
-//            model.addAttribute("code", (String) jsonObject.get("code"));
-//            model.addAttribute("message", (String) jsonObject.get("message"));
-//        }
-//
-//        return "success";
-//    }
-//
-//    @GetMapping(value = "fail")
-//    public String paymentResult(
-//            Model model,
-//            @RequestParam(value = "message") String message,
-//            @RequestParam(value = "code") Integer code
-//    ) throws Exception {
-//
-//        model.addAttribute("code", code);
-//        model.addAttribute("message", message);
-//
-//        return "fail";
-//    }
 
 }
