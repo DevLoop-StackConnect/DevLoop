acbd07f1da752615e6126d180577b595ace318dd
jiyumi <jiyumi00@naver.com>
2024-11-08 02:01:56 +0900

    test: 파티 생성

--- src/test/java/com/devloop/party/service/PartyServiceTest.java
+++ src/test/java/com/devloop/party/service/PartyServiceTest.java
@@ -1,117 +1,62 @@
 package com.devloop.party.service;
 
-import com.devloop.attachment.entity.PartyAttachment;
-import com.devloop.attachment.enums.FileFormat;
-import com.devloop.attachment.s3.S3Service;
-import com.devloop.attachment.service.PartyAttachmentService;
 import com.devloop.common.AuthUser;
+import com.devloop.common.enums.Category;
 import com.devloop.party.entity.Party;
+import com.devloop.party.enums.PartyStatus;
 import com.devloop.party.repository.PartyRepository;
 import com.devloop.party.request.SavePartyRequest;
-import com.devloop.party.request.UpdatePartyRequest;
-import com.devloop.party.response.GetPartyDetailResponse;
 import com.devloop.party.response.SavePartyResponse;
 import com.devloop.user.entity.User;
 import com.devloop.user.enums.UserRole;
-import com.devloop.user.service.UserService;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.util.Optional;
-
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyLong;
-import static org.mockito.Mockito.*;
+import static org.mockito.BDDMockito.given;
 
 
 @ExtendWith(MockitoExtension.class)
 class PartyServiceTest {
+    @InjectMocks
+    private PartyService partyService;
+
     @Mock
     private PartyRepository partyRepository;
-    @Mock
-    private UserService userService;
-    @Spy
-    private PasswordEncoder passwordEncoder;
-    @Mock
-    private S3Service s3Service;
-    @Mock
-    private PartyAttachmentService partyAttachmentService;
-    @InjectMocks
-    public PartyService partyService;
 
-    private AuthUser authUser;
-    private User user;
-    private Party party;
-    private SavePartyRequest savePartyRequest;
-    private UpdatePartyRequest updatePartyRequest;
-    private PartyAttachment partyAttachment;
+    @Mock
     private MultipartFile file;
 
-//    @BeforeEach
-//    public void setUp() throws MalformedURLException {
-//        authUser=new AuthUser(1L,"abc@email.com",UserRole.ROLE_USER);
-//        savePartyRequest=new SavePartyRequest("파티 제목","파티 내용","IN_PROGRESS","WEB_DEV");
-//        updatePartyRequest=new UpdatePartyRequest("수정된 파티 제목","수정된 파티 내용","IN_PROGRESS","WEB_DEV");
-//        user=User.of("홍길동","abc@eamil.com",passwordEncoder.encode("Abc1234!"),UserRole.ROLE_USER);
-//        party=Party.from(savePartyRequest,user);
-//        String imageURL="https://example.com/image.PNG";
-//        file=mock(MultipartFile.class);
-//        partyAttachment=PartyAttachment.of(1L, new URL(imageURL), FileFormat.PNG,"image.PNG");
-//    }
     @Test
-    public void 파티_저장_정상동작(){
+    public void 파티_생성(){
         //given
-        when(userService.findByUserId(anyLong())).thenReturn(user);
-        when(partyRepository.save(any(Party.class))).thenReturn(party);
-        when(file.isEmpty()).thenReturn(false);
+        AuthUser authUser=new AuthUser(1L,"test@email.com", UserRole.ROLE_USER);
+        SavePartyRequest savePartyRequest=SavePartyRequest.of(
+                "제목",
+                "내용",
+                PartyStatus.COMPLETED,
+                Category.WEB_DEV
+        );
+        User user= User.of("홍길동","Abc123!","test@email.com",UserRole.ROLE_USER);
+        Party newParty= Party.from(savePartyRequest,user);
+
+        //mocking
+        given(partyRepository.save(any())).willReturn(newParty);
+        given(file.isEmpty()).willReturn(false);
 
         //when
-        SavePartyResponse response=partyService.saveParty(authUser,file,savePartyRequest);
+        SavePartyResponse savePartyResponse=partyService.saveParty(authUser,file,savePartyRequest);
 
         //then
-        Assertions.assertEquals(party.getId(),response.getPartyId());
-        Assertions.assertEquals(party.getTitle(),response.getTitle());
-        verify(s3Service,times(1)).uploadFile(eq(file),eq(user),any(Party.class));
-    }
-
-    @Test
-    public void 파티_수정_정상동작() {
+        Assertions.assertNotNull(savePartyResponse);
+        Assertions.assertEquals(newParty.getId(),savePartyResponse.getPartyId());
 
     }
 
-    @Test
-    public void 파티_다건조회_정상동작(){
-
-    }
-
-    @Test
-    public void 파티_단건조회_정상동작() throws MalformedURLException {
-        //given
-        Long partyId=1L;
-        when(partyRepository.findById(anyLong())).thenReturn(Optional.of(party));
-       // when(partyAttachmentService.findPartyAttachmentByPartyId(anyLong())).thenReturn(partyAttachment);
-
-        //when
-        GetPartyDetailResponse response=partyService.getParty(partyId);
-        
-        //then
-        Assertions.assertNotNull(response);
-        Assertions.assertEquals(party.getId(),response.getPartyId());
-        Assertions.assertEquals(partyAttachment.getImageURL(),new URL(response.getImageUrl()));
-    }
-
-    @Test
-    public void 파티_삭제_정상동작(){
-
-    }
 }
