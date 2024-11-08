9062699677d5a543b0679e732f4c2e7cd44a2c89
goodsoundisme <sol000619@naver.com>
2024-11-08 09:41:55 +0900

    [ADD] TEST CLASS

---
+++ src/test/java/com/devloop/community/service/CommunityServiceTest.java
@@ -0,0 +1,36 @@
+package com.devloop.community.service;
+
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class CommunityServiceTest {
+
+
+
+
+    @Test
+    public void 커뮤니티글_작성_성공(){ }
+
+    @Test
+    public void 커뮤니티글_다건조회_성공(){ }
+
+    @Test
+    public void 커뮤니티글_단건조회_성공(){ }
+
+    @Test
+    public void 커뮤니티글_수정_성공(){ }
+
+    @Test
+    public void 커뮤니티글_삭제_성공(){ }
+
+    @Test
+    public void 다른사람글_수정_실패(){ }
+
+    @Test
+    public void 다른사람글_삭제_실패(){ }
+
+}
+
