8ac85f8d6832c2f91f0d34d67137da749e67a064
goodsoundisme <sol000619@naver.com>
2024-11-08 09:41:55 +0900

    [ADD] TEST CLASS

---
+++ src/test/java/com/devloop/scheduletodo/service/ScheduleTodoServiceTest.java
@@ -0,0 +1,44 @@
+package com.devloop.scheduletodo.service;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class ScheduleTodoServiceTest {
+
+
+
+
+    @Test
+    public void 일정_작성_성공(){ }
+
+    @Test
+    public void 일정_다건조회_성공(){ }
+
+    @Test
+    public void 일정_단건조회_성공(){ }
+
+    @Test
+    public void 일정_수정_성공(){ }
+
+    @Test
+    public void 튜터_일반유저글_수정_성공(){ }
+
+    @Test
+    public void 튜터_일반유저글_삭제_성공(){ }
+
+    @Test
+    public void 일정_삭제_성공(){ }
+
+    @Test
+    public void 일반유저_다른사람일정_수정_실패(){ }
+
+    @Test
+    public void 일반유저_다른사람일정_삭제_실패(){ }
+
+    @Test
+    public void 미구매자_일정작성_실패(){ }
+
+
+}
